e747d292e45d00276a6f3464a78e1deacd333e17
Vadym Hrynyk <sliderzxc@gmail.com>
2025-06-16 10:23:16 +0200

    add configuration to save secrete keys

---
+++ app/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+
+    })
+    .Build();
+
+await host.RunAsync();
